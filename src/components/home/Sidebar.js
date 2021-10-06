f247a3c7bc66bf6d6bc522ee6e7dbbb656412339
anas1503 <md.anas1503@gmail.com>
2021-10-06 15:28:56 +0530

    change sidebar alignment

--- src/components/home/Sidebar.js
+++ src/components/home/Sidebar.js
@@ -1,16 +1,44 @@
 import React from 'react'
 import './sidebar.css'
+import logo from '../../assets/TMLCLOGO.png'
 
 const Sidebar = () => {
     return (
         <div className="sidebar">
-            <div className="nav">
-                <h3>Home</h3>
-                <h3>Home</h3>
-                <h3>Home</h3>
-                <h3>Home</h3>
-                <h3>Home</h3>
-                <h3>Home</h3>
+        <img src={logo} alt="logo" className="logo"/>
+            <div className="navbar"> 
+            <div class="line">
+                  <hr/>
+                </div>
+                <h3 className="navdata">HOME</h3>
+                <div class="line">
+                  <hr/>
+                </div>
+             
+                <h3 className="navdata">ABOUT</h3>
+                <div class="line">
+                  <hr/>
+                </div>
+             
+                <h3 className="navdata">SERVICES</h3>
+                <div class="line">
+                  <hr/>
+                </div>
+             
+                <h3 className="navdata">TOOLS</h3>
+                <div class="line">
+                  <hr/>
+                </div>
+                  <h3 className="navdata">ACADEMY</h3>
+                <div class="line">
+                  <hr/>
+                </div>
+             
+                <h3 className="navdata">CONTACT</h3>
+      
+                 
+                 
+                
 
             </div>
         </div>
